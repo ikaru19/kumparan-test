36769d1262d1ee753db5cf3ce41a4887e64576dd
m.syafrizal@geniebook.com <m.syafrizal@geniebook.com>
2022-05-08 10:31:44 +0700

    init entity unit testing

--- KumparanTests/KumparanTests.swift
+++ KumparanTests/KumparanTests.swift
@@ -24,6 +24,52 @@ class KumparanTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        test_post_should_produce_post()
+        test_post_should_produce_different_entity()
+        test_user_should_produce_entity()
+        test_user_should_produce_different_entity()
+    }
+
+    func test_post_should_produce_post() {
+        let entity = Domain.PostEntity(id: "1", title: nil, body: nil)
+        XCTAssert(entity != nil)
+    }
+
+    func test_post_should_produce_different_entity() {
+        let entity = Domain.PostEntity(id: "1", title: nil, body: nil)
+        var entity2 = entity
+        entity2.id = "2"
+        XCTAssert(entity.id != entity2.id)
+    }
+
+    func test_user_should_produce_entity() {
+        let entity = Domain.UserEntity(
+                id: "1",
+                name: nil,
+                username: nil,
+                email: nil,
+                phone: nil,
+                website: nil,
+                company: nil,
+                address: nil
+        )
+        XCTAssert(entity != nil)
+    }
+
+    func test_user_should_produce_different_entity() {
+        let entity = Domain.UserEntity(
+                id: "1",
+                name: nil,
+                username: nil,
+                email: nil,
+                phone: nil,
+                website: nil,
+                company: nil,
+                address: nil
+        )
+        var entity2 = entity
+        entity2.id = "2"
+        XCTAssert(entity.id != entity2.id)
     }
 
     func testPerformanceExample() throws {
